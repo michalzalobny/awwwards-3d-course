5cd51be1cbd9ec4a0537b7465084faabfd4808aa
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2021-08-11 21:43:36 +0200

    Add images to about page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['standard'],
+  extends: ["standard"],
   globals: {
-    'IS_DEVELOPMENT': 'readonly',
+    IS_DEVELOPMENT: "readonly",
   },
+  singleQuotes: false,
   parserOptions: {
     ecmaVersion: 2020,
   },
-}
+};
--- app.js
+++ app.js
@@ -1,83 +1,86 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const Prismic = require('prismic-javascript')
-const PrismicDOM = require('prismic-dom')
-const UAParser = require('ua-parser-js')
+const Prismic = require("prismic-javascript");
+const PrismicDOM = require("prismic-dom");
+const UAParser = require("ua-parser-js");
 
-const find = require('lodash/find')
+const find = require("lodash/find");
 
-const Onboarding = require('./onboarding')
+const Onboarding = require("./onboarding");
 
-const app = require('./config')
-const pageSize = 50
+const app = require("./config");
+const pageSize = 50;
 
 const get = (results, request) => {
-  const ua = UAParser(request.headers['user-agent'])
+  const ua = UAParser(request.headers["user-agent"]);
 
-  const meta = find(results, { type: 'metadata' })
+  const meta = find(results, { type: "metadata" });
 
-  const isAJAX = request.headers['x-requested-with'] === 'XMLHttpRequest'
-  const isDesktop = ua.device.type === undefined
-  const isPhone = ua.device.type === 'mobile'
-  const isTablet = ua.device.type === 'tablet'
+  const isAJAX = request.headers["x-requested-with"] === "XMLHttpRequest";
+  const isDesktop = ua.device.type === undefined;
+  const isPhone = ua.device.type === "mobile";
+  const isTablet = ua.device.type === "tablet";
 
   return {
     isAJAX,
     isDesktop,
     isPhone,
     isTablet,
-    meta
-  }
-}
+    meta,
+  };
+};
 
-app.listen(app.get('port'), () => {
-  Onboarding.trigger()
-})
+app.listen(app.get("port"), () => {
+  Onboarding.trigger();
+});
 
 app.use((request, response, next) => {
-  const accessToken = process.env.PRISMIC_ACCESS_TOKEN
-  const endpoint = process.env.PRISMIC_ENDPOINT
+  const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
+  const endpoint = process.env.PRISMIC_ENDPOINT;
 
-  response.locals.PrismicDOM = PrismicDOM
+  response.locals.PrismicDOM = PrismicDOM;
 
   Prismic.api(endpoint, {
     accessToken,
-    request
-  }).then(api => {
-    request.prismic = { api }
-
-    next()
-  }).catch(error => {
-    next(error.message)
+    request,
   })
-})
+    .then((api) => {
+      request.prismic = { api };
 
-app.get('/', (request, response) => {
-  request.prismic.api.query('', { pageSize }).then(({ results }) => {
-    const data = get(results, request)
+      next();
+    })
+    .catch((error) => {
+      next(error.message);
+    });
+});
 
-    response.render('pages/home', { ...data })
-  })
-})
+app.get("/", (request, response) => {
+  request.prismic.api.query("", { pageSize }).then(({ results }) => {
+    const data = get(results, request);
 
-app.get('/about', (request, response) => {
-  request.prismic.api.query('', { pageSize }).then(({ results }) => {
-    const data = get(results, request)
+    response.render("pages/home", { ...data });
+  });
+});
 
-    response.render('pages/about', { ...data })
-  })
-})
+app.get("/about", (request, response) => {
+  request.prismic.api.query("", { pageSize }).then(({ results }) => {
+    const data = get(results, request);
+    const about = find(results, { type: "about" });
+
+    response.render("pages/about", { about, ...data });
+  });
+});
 
 app.use((request, response) => {
-  response.status(404)
+  response.status(404);
 
-  if (request.accepts('html')) {
-    return response.redirect('/')
+  if (request.accepts("html")) {
+    return response.redirect("/");
   }
 
-  if (request.accepts('json')) {
-    return response.send({ error: 'Not Found' })
+  if (request.accepts("json")) {
+    return response.send({ error: "Not Found" });
   }
 
-  response.type('txt').send('Not Found')
-})
+  response.type("txt").send("Not Found");
+});
